a9304dbb8e5191ae8e6c361aad8ee40efbcab997
shyamv1508 <swaminathanveerabathiran2007@gmail.com>
2026-09-19 06:16:32 +0530

    Allow live human gate POST requests

--- api/index.py
+++ api/index.py
@@ -36,7 +36,7 @@ app = FastAPI(title="ATLAS API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["GET"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
